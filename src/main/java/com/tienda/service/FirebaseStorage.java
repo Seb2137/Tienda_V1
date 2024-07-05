57c492cc02a8fbeb397499b4895227bbe9391dae
Seb2137 <settbdwmnw.0@gmail.com>
2024-07-05 10:22:58 -0600

    semana 6

---
+++ src/main/java/com/tienda/service/FirebaseStorage.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.tienda.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface FirebaseStorage {
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+    
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-591a9.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-591a9-firebase-adminsdk-cvuvs-4c9a64301c";
+    
+}
