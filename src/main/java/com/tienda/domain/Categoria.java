d6ab6ff1f89e7f54c67b736050dc1888b97943df
Seb2137 <settbdwmnw.0@gmail.com>
2024-08-05 22:02:12 -0600

    ultimas semanas

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -7,6 +7,7 @@ package com.tienda.domain;
 import jakarta.persistence.*;
 import java.io.Serializable; 
 import lombok.Data;
+import java.util.List;
 
 @Data
 @Entity
@@ -24,6 +25,9 @@ public class Categoria implements Serializable {
   private String rutaImagen;
   private boolean activo;
 
+@OneToMany
+@JoinColumn(name = "id_categoria", updatable=false)
+List<Producto> productos;
 
 public Categoria() {   
 
